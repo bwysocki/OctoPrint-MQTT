a618045abca820e1bc2f3316bac1f448afdcdbde
Bartosz Wysocki <wysockibartosz@gmail.com>
2019-02-04 21:19:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 # coding=utf-8
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "mqtt"
+plugin_identifier = "mqttaws"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_mqtt"
+plugin_package = "octoprint_mqttaws"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-MQTT"
+plugin_name = "OctoPrint-MQTTAWS"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.8.0"
@@ -24,7 +24,7 @@ plugin_author = "Gina Häußge"
 plugin_author_email = "gina@octoprint.org"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/OctoPrint/OctoPrint-MQTT"
+plugin_url = "https://github.com/bwysocki/OctoPrint-MQTT"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
