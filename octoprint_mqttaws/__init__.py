7d577c572db5667679eb42f6d3375733199d39a6
Bartosz Wysocki <bartosz.wysocki@contractors.roche.com>
2019-02-05 20:12:00 +0100

    AWS

--- octoprint_mqttaws/__init__.py
+++ octoprint_mqttaws/__init__.py
@@ -278,6 +278,11 @@ class MqttAWSPlugin(octoprint.plugin.SettingsPlugin,
             myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
             myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
+            self._mqtt = myAWSIoTMQTTClient._mqtt_core._internal_async_client._paho_client
+            self._mqtt.on_connect = self._on_mqtt_connect
+            self._mqtt.on_disconnect = self._on_mqtt_disconnect
+            self._mqtt.on_message = self._on_mqtt_message
+
             myAWSIoTMQTTClient.connect()
 
             # Connect and subscribe to AWS IoT
@@ -287,10 +292,7 @@ class MqttAWSPlugin(octoprint.plugin.SettingsPlugin,
             myAWSIoTMQTTClient.publish(topic, messageJson, 1)
             self._logger.info("INFO 7")
 
-            self._mqtt = myAWSIoTMQTTClient._mqtt_core._internal_async_client._paho_client
-            self._mqtt.on_connect = self._on_mqtt_connect
-            self._mqtt.on_disconnect = self._on_mqtt_disconnect
-            self._mqtt.on_message = self._on_mqtt_message
+
         except:
             self._logger.error("Can not sent sample msg AWS")
 
