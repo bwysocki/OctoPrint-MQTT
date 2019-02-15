9eae8f0e74230bc95976767ce699f2cd739f340c
Bartosz Wysocki <wysockibartosz@gmail.com>
2019-02-15 07:58:07 +0100

    No offline publishing

--- octoprint_mqttaws/__init__.py
+++ octoprint_mqttaws/__init__.py
@@ -307,9 +307,9 @@ class MqttAWSPlugin(octoprint.plugin.SettingsPlugin,
 
         # AWSIoTMQTTClient connection configuration
         myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
-        myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+        myAWSIoTMQTTClient.configureOfflinePublishQueueing(0)  # Infinite offline Publish queueing
         myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
-        myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+        myAWSIoTMQTTClient.configureConnectDisconnectTimeout(30)  # 10 sec
         myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 
         myAWSIoTMQTTClient.onOffline = self._on_mqtt_disconnect
@@ -353,7 +353,7 @@ class MqttAWSPlugin(octoprint.plugin.SettingsPlugin,
                 return True
             else:
                 return False
-        self._mqtt.publish(topic, payload=payload, QoS=qos)
+        self._mqtt.c(topic, payload=payload, QoS=qos)
         self._logger.info("Sent message: {topic} - {payload}".format(**locals()))
         return True
 
