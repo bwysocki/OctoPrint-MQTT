5bdf1221304aed857358c3d981e403421c43216e
Dmitry Ryzhikov <dzmitry.ryzhykau@itechart-group.com>
2019-02-19 12:44:25 +0000

    Merged in feature/linting (pull request #2)
    Feature/linting
    
    * Linting configuration
    
    * Linting fixes
    
    * Add linting step to pipeline
    
    * Install dev dependencies before running linter
    
    Approved-by: Bartosz Wysocki <wysockibartosz@gmail.com>

--- octoprint_mqttaws/test.py
+++ octoprint_mqttaws/test.py
@@ -1,14 +1,12 @@
 import json
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
-import time
-import os
-
 
 host = 'a9tg1a03ro09m-ats.iot.eu-central-1.amazonaws.com'
 rootCAPath = '../../AmazonRootCA1.pem'
 port = 443
 clientId = 'TODO'
 topic = 'abcmqtt'
+
 
 # Custom MQTT message callback
 def customCallback(client, userdata, message):
@@ -18,13 +16,15 @@ def customCallback(client, userdata, message):
     print(message.topic)
     print("--------------\n\n")
 
+
 myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId, useWebsocket=True)
 myAWSIoTMQTTClient.configureEndpoint(host, port)
 myAWSIoTMQTTClient.configureCredentials(rootCAPath)
 
 # AWSIoTMQTTClient connection configuration
 myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
-myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+# Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)
 myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
 myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
 myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
@@ -39,6 +39,6 @@ message['message'] = 'abcada'
 messageJson = json.dumps(message)
 myAWSIoTMQTTClient.publish(topic, messageJson, 1)
 
-a = {"a":1}
+a = {"a": 1}
 
 print myAWSIoTMQTTClient._mqtt_core._internal_async_client._paho_client
