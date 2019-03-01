86bc791c1d1eab82979804718878a6ef16e16d39
Bartosz Wysocki <wysockibartosz@gmail.com>
2019-03-01 10:51:11 +0100

    Version 0.8.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_name = "OctoPrint-MQTTAWS"
 # The plugin's version.
 # Can be overwritten within OctoPrint's internal data via __plugin_version__
 # in the plugin module
-plugin_version = "0.8.1"
+plugin_version = "0.8.2"
 
 # The plugin's description. Can be overwritten within OctoPrint's
 # internal data via __plugin_description__ in the plugin module
